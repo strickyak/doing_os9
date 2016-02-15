060394b29d388c6556c4924c8a85d474b98d7ca3
strick <strick@yak.net>
2016-02-14 16:58:21 -0800

    grep_mods prints Lang & Type.

--- grep_mods/grep_mods.go
+++ grep_mods/grep_mods.go
@@ -8,6 +8,18 @@ import (
 
 const BIG = 100000000
 
+var Types = []string{
+	"???", "Prog", "Subr", "Multi", "Data", "USR 5", "USR 6", "USR 7",
+	"USR 8", "USR 9", "USR A", "USR B", "System", "File Manager",
+	"Device Driver", "Device Descriptor",
+}
+
+var Langs = []string{
+	"Data", "6809 Obj", "Basic09 I-Code", "Pascal P-Code", "C I-Code",
+	"Cobol I-Code", "Fortran I-Code", "6309 Obj", "???", "???", "???",
+	"???", "???", "???", "???", "???",
+}
+
 // TryToDetectModule looks for a module beginning at the
 // start of b.  It checks the header parity, but not the
 // entire module CRC.
@@ -30,8 +42,10 @@ func TryToDetectModule(b []byte, offset int) {
 			k = (int(b[2]) << 8) | int(b[3])
 			fmt.Printf("> at offset 0x%x == %d.\n", offset, offset)
 			fmt.Printf("Size: 0x%x == %d.\n", k, k)
-			fmt.Printf("Type: %d.\n", 15&(b[6]>>4))
-			fmt.Printf("Lang: %d.\n", 15&(b[6]>>0))
+			typ := 15 & (b[6] >> 4)
+			lang := 15 & (b[6] >> 0)
+			fmt.Printf("Type: %d. (%s)\n", typ, Types[typ])
+			fmt.Printf("Lang: %d. (%s)\n", lang, Langs[lang])
 			fmt.Printf("Attr: %d.\n", 15&(b[7]>>4))
 			fmt.Printf("Revn: %d.\n", 15&(b[7]>>0))
 			fmt.Printf("\n")
