03e49955231c10bddef3f38264c8c0f38cb956e7
strick <strick@yak.net>
2016-02-17 12:01:37 -0800

    run2.sh with no trace and no timeout.

---
+++ run2.sh
@@ -0,0 +1,19 @@
+set -ex
+(cd emu; make)
+
+DSK=/home/strick/6809/hg.code.sf.net/nitros9/level1/coco1_yak/nos96809l1coco1_yak_80d.dsk
+
+go run  bootdisk_to_sbc09_file/bootdisk_to_sbc09_file.go < $DSK > /tmp/boot
+
+rm -rf /tmp/disk
+go run grok_os9_disk/grok_os9_disk.go < $DSK  /tmp/disk > _toc
+
+M=/home/strick/6809/hg.code.sf.net/nitros9/level1/coco1_yak/modules
+
+trap 'reset ; reset' 0 1 2 3
+
+# emu/emu -Lff00 -H10000  -0 -i0 -o0 -t -Z 1222333 \
+emu/emu -Lff00 -H10000  -0 -i0 -o0 \
+  -d /tmp/boot \
+  -f $DSK \
+  2>/dev/null
