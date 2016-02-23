312e261b1272b345d1c3d108ccb3445207162fed
strick <strick@yak.net>
2016-02-23 01:37:48 -0800

    progress in 9th compiler

--- compile_9th/compile_9th.go
+++ compile_9th/compile_9th.go
@@ -7,6 +7,7 @@ import "os"
 import "strings"
 
 var F = fmt.Sprintf
+var P = fmt.Printf
 
 type Ninth struct {
 	Lines []string
@@ -14,6 +15,8 @@ type Ninth struct {
 
 	Words []string
 	W     int
+
+	Latest string
 }
 
 func (o *Ninth) NextWord() string {
@@ -55,6 +58,65 @@ func NewNinth(r io.Reader) *Ninth {
 	return &Ninth{Lines: lines}
 }
 
+func EncodeFunnyChars(s string) string {
+	var bb []byte
+	for _, ch := range s {
+		if '0' <= ch && ch <= '9' ||
+			'A' <= ch && ch <= 'Z' ||
+			'a' <= ch && ch <= 'z' {
+			bb = append(bb, byte(ch))
+		} else {
+			bb = append(bb, []byte(F("$%02x", ch))...)
+		}
+	}
+	return string(bb)
+}
+
+func (o *Ninth) DoPrelude(name string, code string) {
+	ename := EncodeFunnyChars(name)
+	ecode := EncodeFunnyChars(code)
+	elatest := EncodeFunnyChars(o.Latest)
+	P("\n\n***  %s  ***\n\n", name)
+	P("l_%s\n", ename)
+	if o.Latest == "" {
+		P("  fcb 0,0 ;link")
+	} else {
+		P("  fcb ($10000+%s-*)/256 ;link\n", elatest)
+		P("  fcb ($10000+%s-*)+1\n", elatest)
+	}
+	P("  fcb %d  ;len\n", len(name))
+	P("  fcc ~%s~\n", name)
+	P("  fcb 0\n")
+
+	P("c_%s\n", ename)
+	P("  fcb ($10000+%s-*)/256 ;link\n", ecode)
+	P("  fcb ($10000+%s-*)+1\n", ecode)
+	P("b_%s\n", ename)
+
+	o.Latest = name
+}
+
+func (o *Ninth) InsertCode() {
+	for {
+		s := o.NextLine()
+		if strings.Trim(s, " \t") == ";" {
+			break
+		}
+		P("%s\n", s)
+	}
+	P("  jmp Next,pcr\n")
+}
+
+func (o *Ninth) DoCode() {
+	name := o.NextWord()
+	o.DoPrelude(name, "c_"+name)
+	o.InsertCode()
+}
+func (o *Ninth) DoColon() {
+	name := o.NextWord()
+	o.DoPrelude(name, "Enter")
+}
+
 func CompileFile(w io.Writer, r io.Reader) {
 	o := NewNinth(r)
 	for {
@@ -62,12 +124,11 @@ func CompileFile(w io.Writer, r io.Reader) {
 		if w == ">EOF<" {
 			break
 		}
-		println("===", w, "===")
 		switch w {
 		case ":":
-			DoColon(o)
+			o.DoColon()
 		case "code":
-			DoCode(o)
+			o.DoCode()
 		default:
 			panic(F("Unknown Command: %q", w))
 		}
