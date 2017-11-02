1f5b6ad74fd63288f96077cd9945889116b3793c
strick <strick@yak.net>
2017-11-02 14:54:20 -0700

    Make tmp copy of disk, in run2.sh

--- run2.sh
+++ run2.sh
@@ -2,6 +2,8 @@ set -ex
 (cd emu; make)
 
 DSK=/home/strick/6809/hg.code.sf.net/nitros9/level1/coco1_yak/nos96809l1coco1_yak_80d.dsk
+DSK2=/tmp/dsk2.dsk
+cp $DSK $DSK2
 
 go run  bootdisk_to_sbc09_file/bootdisk_to_sbc09_file.go < $DSK > /tmp/boot
 
@@ -15,5 +17,5 @@ M=/home/strick/6809/hg.code.sf.net/nitros9/level1/coco1_yak/modules
 # emu/emu -Lff00 -H10000  -0 -i0 -o0 -t -Z 1222333 \
 emu/emu -Lff00 -H10000  -0 -i0 -o0 \
   -d /tmp/boot \
-  -f $DSK \
+  -f $DSK2 \
   2>/dev/null
