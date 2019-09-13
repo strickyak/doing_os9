0ce93176592a98661fe6d5a91d5a783b679df7d8
strick <strick@yak.net>
2019-09-12 23:13:24 -0700

    Flag for --level=2 and level2 irq vectors

--- bootdisk_to_sbc09_file/bootdisk_to_sbc09_file.go
+++ bootdisk_to_sbc09_file/bootdisk_to_sbc09_file.go
@@ -3,9 +3,14 @@
 //   go run bootdisk_to_sbc09_file.go <  $HOME/6809/nitros9/nos96809l1v030208coco_80d.dsk > /tmp/boot
 package main
 
-import "bytes"
-import "io"
-import "os"
+import (
+	 "bytes"
+	 "flag"
+	 "io"
+	 "os"
+)
+
+var flagLevel = flag.Int("level", 1, "level 1 or 2, for interrupt vectors")
 
 const BOOT_SECTOR = 1224
 
@@ -20,6 +25,7 @@ func PutWord(a uint16, x uint16) {
 }
 
 func main() {
+	flag.Parse()
 	Mem = make([]byte, 0x10000)
 
 	// Emit "JMP $2602" at $100.
@@ -28,12 +34,24 @@ func main() {
 	PutWord(0x0103, 0x2602) // ... $2602
 
   // Set COCO Interrupt Vectors.
-	PutWord(0xFFF2, 0x0100) // SWI3
-	PutWord(0xFFF4, 0x0103) // SWI2
-	PutWord(0xFFFA, 0x0106) // SWI
-	PutWord(0xFFFC, 0x0109) // NMI
-	PutWord(0xFFF8, 0x010C) // IRQ
-	PutWord(0xFFF6, 0x010F) // FIRQ
+	switch *flagLevel {
+	case 1:
+		PutWord(0xFFF2, 0x0100) // SWI3
+		PutWord(0xFFF4, 0x0103) // SWI2
+		PutWord(0xFFFA, 0x0106) // SWI
+		PutWord(0xFFFC, 0x0109) // NMI
+		PutWord(0xFFF8, 0x010C) // IRQ
+		PutWord(0xFFF6, 0x010F) // FIRQ
+	case 2:
+		PutWord(0xFFF2, 0xFEEE) // SWI3
+		PutWord(0xFFF4, 0xFEF1) // SWI2
+		PutWord(0xFFFA, 0xFEFA) // SWI
+		PutWord(0xFFFC, 0xFEFD) // NMI
+		PutWord(0xFFF8, 0xFEF7) // IRQ
+		PutWord(0xFFF6, 0xFEF4) // FIRQ
+	default:
+		panic("bad level")
+	}
 
 	// Read 18 256-byte sectors, starting at BOOT_SECTOR.
 	_, err := os.Stdin.Seek(BOOT_SECTOR*256, 0)
