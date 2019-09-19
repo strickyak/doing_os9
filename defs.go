3c1209c05461ff625e194ddb55608c86309076b1
strick <strick@yak.net>
2019-09-18 19:13:30 -0700

    show multiple modules in file.  gofmt.

--- defs.go
+++ defs.go
@@ -4,114 +4,129 @@ import "fmt"
 import "io"
 
 type Record struct {
-  Len int
-  Type string
-  Field string
-  Remark string
+	Len    int
+	Type   string
+	Field  string
+	Remark string
 }
 
 func FindString(recs []Record, b []byte, field string) string {
-  k := 0
-  for _, rec := range recs {
-    if rec.Field == field {
-      switch rec.Type {
-        case "S": {
-          z := ""
-          for i := 0; i < rec.Len; i++ {
-            z += string([]byte{b[k+i]&127})
-            if b[k+i]&128 != 0 { break }
-          }
-          return z
-        }
-        default:
-          panic(fmt.Sprintf("Field %q wrong type for String: %q", field, rec.Type))
-      }
-    }
-    k += rec.Len
-  }
-  panic(fmt.Sprintf("Field %q not found", field))
+	k := 0
+	for _, rec := range recs {
+		if rec.Field == field {
+			switch rec.Type {
+			case "S":
+				{
+					z := ""
+					for i := 0; i < rec.Len; i++ {
+						z += string([]byte{b[k+i] & 127})
+						if b[k+i]&128 != 0 {
+							break
+						}
+					}
+					return z
+				}
+			default:
+				panic(fmt.Sprintf("Field %q wrong type for String: %q", field, rec.Type))
+			}
+		}
+		k += rec.Len
+	}
+	panic(fmt.Sprintf("Field %q not found", field))
 }
 
 func FindInt(recs []Record, b []byte, field string) int {
-  k := 0
-  for _, rec := range recs {
-    if rec.Field == field {
-      switch rec.Type {
-        case "@": {
-          return k
-        }
-        case "d", "A": {
-          z := 0
-          for i := 0; i < rec.Len; i++ {
-            z = (z<<8) | int(b[k+i])
-          }
-          return z
-        }
-        default:
-          panic(fmt.Sprintf("Field %q wrong type for Int: %q", field, rec.Type))
-      }
-    }
-    k += rec.Len
-  }
-  panic(fmt.Sprintf("Field %q not found", field))
+	k := 0
+	for _, rec := range recs {
+		if rec.Field == field {
+			switch rec.Type {
+			case "@":
+				{
+					return k
+				}
+			case "d", "A":
+				{
+					z := 0
+					for i := 0; i < rec.Len; i++ {
+						z = (z << 8) | int(b[k+i])
+					}
+					return z
+				}
+			default:
+				panic(fmt.Sprintf("Field %q wrong type for Int: %q", field, rec.Type))
+			}
+		}
+		k += rec.Len
+	}
+	panic(fmt.Sprintf("Field %q not found", field))
 }
 
 func PrintRecords(recs []Record, b []byte) {
-  k := 0
-  for _, rec := range recs {
-    fmt.Printf("%s:\t", rec.Field)
-    for i := 0; i < rec.Len; i++ {
-      fmt.Printf("%02x ", b[k+i])
-    }
-    fmt.Printf("  \t[%d] %q :: ", rec.Len, rec.Remark)
+	k := 0
+	for _, rec := range recs {
+		fmt.Printf("%s:\t", rec.Field)
+		for i := 0; i < rec.Len; i++ {
+			fmt.Printf("%02x ", b[k+i])
+		}
+		fmt.Printf("  \t[%d] %q :: ", rec.Len, rec.Remark)
 
-    switch rec.Type {
-    case "d": {
-      z := 0
-      for i := 0; i < rec.Len; i++ {
-        z = (z<<8) | int(b[k+i])
-      }
-      fmt.Printf("%d", z)
-    }
-    case "S": {
-      z := ""
-      for i := 0; i < rec.Len; i++ {
-        z += string([]byte{b[k+i]&127})
-        if b[k+i]&128 != 0 { break }
-      }
-      fmt.Printf("%q", z)
-    }
-    case "T": {
-      switch rec.Len {
-      case 5:
-        fmt.Printf("%04d-%02d-%02d %02d:%02d", 1900+int(b[k]), b[k+1], b[k+2], b[k+3], b[k+4])
-      case 3:
-        fmt.Printf("%04d-%02d-%02d", 1900+int(b[k]), b[k+1], b[k+2])
-      default:
-        panic("bad len")
-      }
-    }
-    case "A": {
-      fmt.Printf("(%s)", FileAttrs(int(b[k])))
-    }
-    case "@": {
-      fmt.Printf("@%d", k)
-    }
-    case "-": {
-      fmt.Printf("--")
-    }
-    }
+		switch rec.Type {
+		case "d":
+			{
+				z := 0
+				for i := 0; i < rec.Len; i++ {
+					z = (z << 8) | int(b[k+i])
+				}
+				fmt.Printf("%d", z)
+			}
+		case "S":
+			{
+				z := ""
+				for i := 0; i < rec.Len; i++ {
+					z += string([]byte{b[k+i] & 127})
+					if b[k+i]&128 != 0 {
+						break
+					}
+				}
+				fmt.Printf("%q", z)
+			}
+		case "T":
+			{
+				switch rec.Len {
+				case 5:
+					fmt.Printf("%04d-%02d-%02d %02d:%02d", 1900+int(b[k]), b[k+1], b[k+2], b[k+3], b[k+4])
+				case 3:
+					fmt.Printf("%04d-%02d-%02d", 1900+int(b[k]), b[k+1], b[k+2])
+				default:
+					panic("bad len")
+				}
+			}
+		case "A":
+			{
+				fmt.Printf("(%s)", FileAttrs(int(b[k])))
+			}
+		case "@":
+			{
+				fmt.Printf("@%d", k)
+			}
+		case "-":
+			{
+				fmt.Printf("--")
+			}
+		}
 
-    fmt.Printf("\n")
-    k += rec.Len
-  }
+		fmt.Printf("\n")
+		k += rec.Len
+	}
 }
 
 func ReadN(r io.Reader, n int) []byte {
-  z := make([]byte, n, n)
-  _, err := io.ReadFull(r, z)
-  if err != nil { panic(err) }
-  return z
+	z := make([]byte, n, n)
+	_, err := io.ReadFull(r, z)
+	if err != nil {
+		panic(err)
+	}
+	return z
 }
 
 /*
@@ -127,14 +142,14 @@ func ReadN(r io.Reader, n int) []byte {
   ISIZ.          EQU       %00100000
 */
 func FileAttrs(x int) string {
-  z := []byte("dsiXWRxwr")
-  var i uint
-  for i = 0; i < 8; i++ {
-    if (1 << i) & x == 0 {
-      z[7-i] = '-'
-    }
-  }
-  return string(z)
+	z := []byte("dsiXWRxwr")
+	var i uint
+	for i = 0; i < 8; i++ {
+		if (1<<i)&x == 0 {
+			z[7-i] = '-'
+		}
+	}
+	return string(z)
 }
 
 // ***********************
@@ -162,26 +177,25 @@ func FileAttrs(x int) string {
 //               DD.NAM         RMB       32                  Volume name
 //               DD.OPT         RMB       32                  Option area
 
-var Sector0 = []Record {
-  {  3, "d", "DD.TOT", "Total number of sectors" },
-  {  1, "d", "DD.TKS", "Track size in sectors" },
-  {  2, "d", "DD.MAP", "Number of bytes in allocation bit map" },
-  {  2, "d", "DD.BIT", "Number of sectors/bit" },
-  {  3, "d", "DD.DIR", "Address of root directory fd" },
-  {  2, "d", "DD.OWN", "Owner" },
-  {  1, "d", "DD.ATT", "Attributes" },
-  {  2, "d", "DD.DSK", "Disk ID" },
-  {  1, "d", "DD.FMT", "Disk format; density/sides" },
-  {  2, "d", "DD.SPT", "Sectors/track" },
-  {  2, "d", "DD.RES", "Reserved for future use" },
-  {  0, "@", "DD.SIZ", "Device descriptor minimum size" },
-  {  3, "d", "DD.BT",  "System bootstrap sector" },
-  {  2, "d", "DD.BSZ", "Size of system bootstrap" },
-  {  5, "T", "DD.DAT", "Creation date" },
-  { 32, "S", "DD.NAM", "Volume name" },
-  { 32, "-", "DD.OPT", "Option area" },
+var Sector0 = []Record{
+	{3, "d", "DD.TOT", "Total number of sectors"},
+	{1, "d", "DD.TKS", "Track size in sectors"},
+	{2, "d", "DD.MAP", "Number of bytes in allocation bit map"},
+	{2, "d", "DD.BIT", "Number of sectors/bit"},
+	{3, "d", "DD.DIR", "Address of root directory fd"},
+	{2, "d", "DD.OWN", "Owner"},
+	{1, "d", "DD.ATT", "Attributes"},
+	{2, "d", "DD.DSK", "Disk ID"},
+	{1, "d", "DD.FMT", "Disk format; density/sides"},
+	{2, "d", "DD.SPT", "Sectors/track"},
+	{2, "d", "DD.RES", "Reserved for future use"},
+	{0, "@", "DD.SIZ", "Device descriptor minimum size"},
+	{3, "d", "DD.BT", "System bootstrap sector"},
+	{2, "d", "DD.BSZ", "Size of system bootstrap"},
+	{5, "T", "DD.DAT", "Creation date"},
+	{32, "S", "DD.NAM", "Volume name"},
+	{32, "-", "DD.OPT", "Option area"},
 }
-
 
 //************************
 //* File Descriptor Format
@@ -207,15 +221,14 @@ var Sector0 = []Record {
 //FD.LS2         EQU       (256/FDSL.S-1)*FDSL.S
 //MINSEC         SET       16
 
-
-var FileDescSector = []Record {
-  {  1, "A", "FD.ATT", "Attributes" },
-  {  2, "d", "FD.OWN", "Owner" },
-  {  5, "T", "FD.DAT", "Date last modified" },
-  {  1, "d", "FD.LNK", "Link count" },
-  {  4, "d", "FD.SIZ", "File size" },
-  {  3, "T", "FD.Creat", "File creation date (YY/MM/DD)" },
-  {  0, "@", "FD.SEG", "Beginning of segment list" },
+var FileDescSector = []Record{
+	{1, "A", "FD.ATT", "Attributes"},
+	{2, "d", "FD.OWN", "Owner"},
+	{5, "T", "FD.DAT", "Date last modified"},
+	{1, "d", "FD.LNK", "Link count"},
+	{4, "d", "FD.SIZ", "File size"},
+	{3, "T", "FD.Creat", "File creation date (YY/MM/DD)"},
+	{0, "@", "FD.SEG", "Beginning of segment list"},
 }
 
 //* Segment List Entry Format
@@ -227,11 +240,10 @@ var FileDescSector = []Record {
 //FD.LS2         EQU       (256/FDSL.S-1)*FDSL.S
 //MINSEC         SET       16
 
-var SegmentEntry = []Record {
-  {  3, "d", "FDSL.A", "Segment beginning physical sector number" },
-  {  2, "d", "FDSL.B", "Segment size" },
+var SegmentEntry = []Record{
+	{3, "d", "FDSL.A", "Segment beginning physical sector number"},
+	{2, "d", "FDSL.B", "Segment size"},
 }
-
 
 //************************
 //* Directory Entry Format
@@ -244,46 +256,61 @@ var SegmentEntry = []Record {
 //DIR.FD         RMB       3                   File descriptor physical sector number
 //DIR.SZ         EQU       .                   Directory record size
 
-var DirEntry = []Record {
-  { 29, "S", "DIR.NM", "File name" },
-  {  3, "d", "DIR.FD", "File descriptor physical sector number" },
-  {  0, "@", "DIR.SZ", "Directory record size" },
+var DirEntry = []Record{
+	{29, "S", "DIR.NM", "File name"},
+	{3, "d", "DIR.FD", "File descriptor physical sector number"},
+	{0, "@", "DIR.SZ", "Directory record size"},
 }
 
 func AsciiString(a []byte) string {
-  var z []byte
-  for i := 0; i < len(a); i++ {
-    if a[i] == 0 { break }
-    z = append(z, a[i]&127)
-    if a[i]&128 != 0 { break }
-  }
-  return string(z)
+	var z []byte
+	for i := 0; i < len(a); i++ {
+		if a[i] == 0 {
+			break
+		}
+		z = append(z, a[i]&127)
+		if a[i]&128 != 0 {
+			break
+		}
+	}
+	return string(z)
 }
 func Int(a []byte, n int) int {
-  z := 0
-  for i := 0; i < n; i++ {
-    z = (z << 8) | int(a[i])
-  }
-  return z
+	z := 0
+	for i := 0; i < n; i++ {
+		z = (z << 8) | int(a[i])
+	}
+	return z
 }
 
 func PrintModuleHeader(a []byte) {
-  if len(a) < 9 || a[0] != 0x87 || a[1] != 0xCD {
-    fmt.Printf("{Not a module.}\n")
-    return
-  }
-  size := Int(a[2:], 2)
-  nameAt := Int(a[4:], 2)
-  name := AsciiString(a[nameAt:])
-  typ := a[6]
-  attrs := (a[7] >> 4) & 15
-  rev := a[7] & 15
+	offset := 0
+	for {
+		if len(a) < 9 || a[0] != 0x87 || a[1] != 0xCD {
+			fmt.Printf("{Not a module.}\n")
+			return
+		}
+		size := Int(a[2:], 2)
+		nameAt := Int(a[4:], 2)
+		name := AsciiString(a[nameAt:])
+		typ := a[6]
+		attrs := (a[7] >> 4) & 15
+		rev := a[7] & 15
 
-  fmt.Printf("{Module: %q size %d type %d attrs %d rev %d}\n", name, size, typ, attrs, rev)
-  exec := Int(a[9:], 2)
-  stack := Int(a[11:], 2)
-  caps := Int(a[13:], 1)
-  fmt.Printf("{    exec_offset %d stack_req %d driver_caps %d}", exec, stack, caps)
+		fmt.Printf("{Module: %q size %d type %d attrs 0x%02x rev %d}\n", name, size, typ, attrs, rev)
+		exec := Int(a[9:], 2)
+		stack := Int(a[11:], 2)
+		caps := Int(a[13:], 1)
+		fmt.Printf("{    exec_offset %d stack_req %d driver_caps 0x%02x}\n", exec, stack, caps)
+
+		if len(a) > size+10 {
+			a = a[size:]
+			offset += size
+			fmt.Printf("{next module offset %d}\n", offset)
+		} else {
+			break
+		}
+	}
 }
 
 /*
--- grok_os9_disk/grok_os9_disk.go
+++ grok_os9_disk/grok_os9_disk.go
@@ -20,117 +20,124 @@ import "os"
 import FP "path/filepath"
 
 type Segment struct {
-  At int
-  Segs int
+	At   int
+	Segs int
 }
 type PairIS struct {
-  A int
-  B string
+	A int
+	B string
 }
 
 func main() {
 
-  bb := D.ReadN(os.Stdin, 256)
+	bb := D.ReadN(os.Stdin, 256)
 
-  fmt.Println("=== SECTOR ZERO ===\n")
-  D.PrintRecords(D.Sector0, bb)
-  fmt.Println("\n")
+	fmt.Println("=== SECTOR ZERO ===\n")
+	D.PrintRecords(D.Sector0, bb)
+	fmt.Println("\n")
 
-  rootInode := D.FindInt(D.Sector0, bb, "DD.DIR")
-  PrintInode(rootInode, "/")
+	rootInode := D.FindInt(D.Sector0, bb, "DD.DIR")
+	PrintInode(rootInode, "/")
 }
 
 func ReadAtLen(pos int, sz int) []byte {
-  _, err := os.Stdin.Seek(int64(pos), 0)
-  if err != nil {panic(err)}
-  return D.ReadN(os.Stdin, sz)
+	_, err := os.Stdin.Seek(int64(pos), 0)
+	if err != nil {
+		panic(err)
+	}
+	return D.ReadN(os.Stdin, sz)
 }
 func PrintInode(inode int, path string) {
-  bb := ReadAtLen(inode*256, 256)
+	bb := ReadAtLen(inode*256, 256)
 
-  fmt.Printf("\n========= INODE #%d %q =========\n\n", inode, path)
-  D.PrintRecords(D.FileDescSector, bb)
-  fmt.Println("\n")
-  attrs := D.FindInt(D.FileDescSector, bb, "FD.ATT")
-  size := D.FindInt(D.FileDescSector, bb, "FD.SIZ")
+	fmt.Printf("\n========= INODE #%d %q =========\n\n", inode, path)
+	D.PrintRecords(D.FileDescSector, bb)
+	fmt.Println("\n")
+	attrs := D.FindInt(D.FileDescSector, bb, "FD.ATT")
+	size := D.FindInt(D.FileDescSector, bb, "FD.SIZ")
 
-  var segments []Segment
-  for i := 0; 16 + 5*i < 256-5+1; i++ {
-    bb = ReadAtLen(inode*256 + 16 + 5*i, 256)
-    at := D.FindInt(D.SegmentEntry, bb, "FDSL.A")
-    segs := D.FindInt(D.SegmentEntry, bb, "FDSL.B")
-    if segs == 0 { continue }
+	var segments []Segment
+	for i := 0; 16+5*i < 256-5+1; i++ {
+		bb = ReadAtLen(inode*256+16+5*i, 256)
+		at := D.FindInt(D.SegmentEntry, bb, "FDSL.A")
+		segs := D.FindInt(D.SegmentEntry, bb, "FDSL.B")
+		if segs == 0 {
+			continue
+		}
 
-    // Grr. Some flawed disks let me this heuristic hack.
-    if at > 1000000 || segs > 1000 {
-      fmt.Printf("ERROR: FLAWED SEGMENT TABLE: %d %d\n", at, segs)
-      break
-    }
+		// Grr. Some flawed disks let me this heuristic hack.
+		if at > 1000000 || segs > 1000 {
+			fmt.Printf("ERROR: FLAWED SEGMENT TABLE: %d %d\n", at, segs)
+			break
+		}
 
+		segments = append(segments, Segment{at, segs})
+		fmt.Printf("=== Segment %d ===\n", i)
+		D.PrintRecords(D.SegmentEntry, bb)
+	}
+	fmt.Println("\n")
 
-    segments = append(segments, Segment{at, segs})
-    fmt.Printf("=== Segment %d ===\n", i)
-    D.PrintRecords(D.SegmentEntry, bb)
-  }
-  fmt.Println("\n")
+	if attrs&0x80 != 0 { // If is a DIR
 
-  if attrs & 0x80 != 0 {  // If is a DIR
+		var subs []PairIS
+		for _, p := range segments {
+			fmt.Printf("=== Directory Segment at %d segs %d (inode %d path %q) ===\n", p.At, p.Segs, inode, path)
 
-    var subs []PairIS
-    for _, p := range segments {
-      fmt.Printf("=== Directory Segment at %d segs %d (inode %d path %q) ===\n", p.At, p.Segs, inode, path)
+			if len(os.Args) > 1 {
+				os.MkdirAll(FP.Join(os.Args[1], path), 0755)
+			}
 
-      if len(os.Args) > 1 {
-        os.MkdirAll(FP.Join(os.Args[1], path), 0755)
-      }
+			segsize := p.Segs * 256
+			if size < segsize {
+				segsize, size = size, 0
+			} else {
+				size -= segsize
+			}
 
-      segsize := p.Segs*256
-      if size < segsize {
-        segsize, size = size, 0
-      } else {
-        size -= segsize
-      }
+			nth := 0
+			for i := 0; i*32 < segsize; i++ {
+				bb = ReadAtLen(p.At*256+i*32, 32)
+				fd := D.FindInt(D.DirEntry, bb, "DIR.FD")
+				if fd == 0 {
+					continue
+				}
+				D.PrintRecords(D.DirEntry, bb)
+				name := D.FindString(D.DirEntry, bb, "DIR.NM")
+				// Grr. Some flawed disks led me to the "nth >= 2" heuristic hack.
+				if name != "." && name != ".." && nth >= 2 {
+					subName := path + "/" + name
+					if path == "/" {
+						subName = "/" + name
+					}
+					subs = append(subs, PairIS{fd, subName})
+				}
+				nth++
+			}
+		}
+		for _, p := range subs {
+			PrintInode(p.A, p.B)
+		}
 
-      nth := 0
-      for i := 0; i*32 < segsize; i++ {
-        bb = ReadAtLen(p.At*256 + i*32, 32)
-        fd := D.FindInt(D.DirEntry, bb, "DIR.FD")
-        if fd == 0 { continue }
-        D.PrintRecords(D.DirEntry, bb)
-        name := D.FindString(D.DirEntry, bb, "DIR.NM")
-        // Grr. Some flawed disks led me to the "nth >= 2" heuristic hack.
-        if name != "." && name != ".." && nth >= 2 {
-          subName := path + "/" + name
-          if path == "/" { subName = "/" + name }
-          subs = append(subs, PairIS{fd, subName})
-        }
-        nth++
-      }
-    }
-    for _, p := range subs {
-      PrintInode(p.A, p.B)
-    }
+	} else { // IF is a FILE
+		var contents []byte
+		for _, p := range segments {
+			fmt.Printf("=== File Segment at %d segs %d (inode %d path %q) ===\n", p.At, p.Segs, inode, path)
 
-  } else {  // IF is a FILE
-    var contents []byte
-    for _, p := range segments {
-      fmt.Printf("=== File Segment at %d segs %d (inode %d path %q) ===\n", p.At, p.Segs, inode, path)
+			segsize := p.Segs * 256
+			if size < segsize {
+				segsize, size = size, 0
+			} else {
+				size -= segsize
+			}
 
-      segsize := p.Segs*256
-      if size < segsize {
-        segsize, size = size, 0
-      } else {
-        size -= segsize
-      }
+			bb := ReadAtLen(p.At*256, segsize)
+			contents = append(contents, bb...)
+		}
 
-      bb := ReadAtLen(p.At*256, segsize)
-      contents = append(contents, bb...)
-    }
+		D.PrintModuleHeader(contents)
 
-    D.PrintModuleHeader(contents)
-
-    if len(os.Args) > 1 {
-      ioutil.WriteFile(FP.Join(os.Args[1], path), contents, 0666)
-    }
-  }
+		if len(os.Args) > 1 {
+			ioutil.WriteFile(FP.Join(os.Args[1], path), contents, 0666)
+		}
+	}
 }
