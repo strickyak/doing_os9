88a0ac3d6dae9bab060b973886ce1990cff6df0e
strick <strick@yak.net>
2021-07-26 00:49:53 -0700

    cmocly for getting listings from cmoc

---
+++ gomar/cmocly/run.sh
@@ -0,0 +1,8 @@
+go run cmocly.go \
+    --linker_map temp.map \
+    --asm_listing octet.list:defs.list \
+    --asm_listing_path /home/strick/COCO/build/cmoc-rebuild/src/stdlib \
+    --
+
+#
+#    --asm_listing_path /home/strick/COCO/build/cmoc-0.1.61/src/stdlib/ \
