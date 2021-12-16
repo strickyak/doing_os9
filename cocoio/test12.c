90305bf86acb80391556552dfff4cb364ec9d14d
strick <strick@yak.net>
2021-12-15 20:25:45 -0800

    My attempt at a CocoIO test.

--- cocoio/test12.c
+++ cocoio/test12.c
@@ -1,44 +1,60 @@
-// Test for the CoCoIO with the WizNet 5100s chip
+// Test12 for the CoCoIO with the WizNet 5100s chip.
 //   https://computerconect.com/products/cocoio-network-only-developers-edition
 // Copying the logic from
 //   github.com/MarkO-555/CoCoIO-NIC-Base-Code/BASIC/COCOIO12.BAS
 // C code by Strick.
 //   MIT license (see file `LICENSE`).
-//   Hint: /opt/yak/cmoc/bin/cmoc --os9 test12.c
+
+//   Hint:
+//     /opt/yak/cmoc/bin/cmoc --os9 test12.c
+//     os9 makdir ~/cocosdc/E.DSK,work
+//     os9 copy -l test12.c ~/cocosdc/E.DSK,work/test12.c
+//     os9 copy test12 ~/cocosdc/E.DSK,work/test12
+//     os9 attr -per ~/cocosdc/E.DSK,work/test12
 
 #include <cmoc.h>
 #include <assert.h>
 
 typedef unsigned char byte;
-typedef int word;
-typedef int addr;
+typedef unsigned int word;
+typedef unsigned int addr;
 
-#define CMD 0xFF68
-#define LOC_HI  0xFF69
-#define LOC_LO  0xFF6A
-#define REG  0xFF6B
+// The chip could be at $FF68 or $FF78.
+#define BASE 0xFF68
+// Four ports for communicating with the chip.
+#define CMD     (BASE)
+#define LOC_HI  (BASE+1)
+#define LOC_LO  (BASE+2)
+#define REG     (BASE+3)
 
 // Extract Hi or Lo byte from a word.
 byte Hi(word a) { return (byte)(0xFF & (a >> 8)); }
 byte Lo(word a) { return (byte)(0xFF & (a)); }
 
+// Is my C program running much faster than the BASIC?
+// Try slowing it down.
+word Temp;
+void Delay(word n) {
+    for (; n; n--) { Temp+=n; }
+}
+
 // Peek and Poke a byte to 6809 memory space.
-byte Peek(addr a) { return *(byte*)a; }
-void Poke(addr a, byte b) { *(byte*)a = b; }
+byte Peek(addr a) { Delay(50); return *(byte*)a; }
+void Poke(addr a, byte b) { Delay(50); *(byte*)a = b; }
 
 // Reset the chip.
 void Reset() { Poke(CMD, 0x80); }
 // Enable auto-incrementing location for Gets and Puts.
 void Auto() { Poke(CMD, 0x03); }
-// Set register location.
+// Set chip's register location.
 void SetLoc(word loc) {
+   Auto();
    Poke(LOC_HI, Hi(loc));
    Poke(LOC_LO, Lo(loc));
 }
-// Get or Put registers.
+// Get or Put chip registers.
 byte Get() { return Peek(REG); }
 void Put(byte b) { Poke(REG, b); }
-
 
 // GWR: Gateway IP Address.
 void SetGateway(byte a, byte b, byte c, byte d) {
@@ -70,6 +86,7 @@ void GetMyIp(byte *a, byte *b, byte *c, byte *d) {
 }
 
 int main() {
+  Reset();
   SetMac(0, 1, 2, 3, 4, 5);
   SetGateway(10, 1, 2, 3);
   SetMyIp(10, 11, 22, 33);
