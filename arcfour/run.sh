d54ac4b8c974faf1dbec71628665731a1b7e256e
strick <strick@yak.net>
2022-02-12 11:30:31 -0800

    use gomar/launch.sh

--- arcfour/run.sh
+++ arcfour/run.sh
@@ -1,9 +1,9 @@
 set -eux
 
-cmoc -o z  --os9 -i "$1" arc4.c ssh-arcfour.c
+PATH="/opt/yak/cmoc/bin:$PATH" cmoc -o z  --os9 -i "$1" arc4.c ssh-arcfour.c
 
-os9 copy -r z ../gomar/drive/disk2,CMDS/z
-os9 attr -per ../gomar/drive/disk2,CMDS/z 
-echo 'z </term' | os9 copy -r -l /dev/stdin ../gomar/drive/disk2,startup
+# os9 copy -r z ../gomar/drive/disk2,CMDS/z
+# os9 attr -per ../gomar/drive/disk2,CMDS/z 
+# echo 'z </term' | os9 copy -r -l /dev/stdin ../gomar/drive/disk2,startup
 
-cd ../gomar ; sh run2.sh
+sh ../gomar/launch.sh z
