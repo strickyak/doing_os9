ad85f11bcce8996a1241482cb53317e82e211b6c
strick <strick@yak.net>
2022-03-21 14:05:42 -0700

    add emu/verbose.go

---
+++ gomar/emu/verbose.go
@@ -0,0 +1,37 @@
+package emu
+
+import (
+	"flag"
+	"log"
+)
+
+// NEW WITH Compile tags
+// `d` Device
+
+// Verbosity:
+//	'a' all lowercase letters
+//	's' sys calls
+//	'r' RAM dumps at sys calls
+//	'd' I/O devices
+//	'i' instructions
+//	'm' memory get/put
+//	'p' physical memory get/put
+var V [128]bool                                                            // Verbosity bits
+var FlagInitialVerbosity = flag.String("v", "", "Initial verbosity chars") // Initial Verbosity
+var FlagTraceVerbosity = flag.String("vv", "", "Trace verbosity chars")    // Trace Verbosity
+var FlagTraceAfter = flag.Uint64("t", MaxUint64, "Tracing starts after this many steps")
+
+func SetVerbosityBits(s string) {
+	for _, r := range s {
+		if int(r) >= len(V) {
+			log.Panicf("Verbosity rune %d too large for Verbosity Array", r)
+		}
+		if r == 'a' {
+			for i := 'a'; i <= 'z'; i++ {
+				V[i] = true
+			}
+			continue
+		}
+		V[r] = true
+	}
+}
