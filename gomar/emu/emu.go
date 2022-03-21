56f06c4d6ce0a625c87af3bf2991d2c64afd43cf
strick <strick@yak.net>
2022-03-21 14:06:48 -0700

    moved verbosity to emu/verbosity.go

--- gomar/emu/emu.go
+++ gomar/emu/emu.go
@@ -74,34 +74,6 @@ func Z(w *bytes.Buffer, format string, args ...interface{}) {
 	fmt.Fprintf(w, format, args...)
 }
 
-// Verbosity:
-//	'a' all lowercase letters
-//	's' sys calls
-//	'r' RAM dumps at sys calls
-//	'd' I/O devices
-//	'i' instructions
-//	'm' memory get/put
-//	'p' physical memory get/put
-var V [128]bool                                                            // Verbosity bits
-var FlagInitialVerbosity = flag.String("v", "", "Initial verbosity chars") // Initial Verbosity
-var FlagTraceVerbosity = flag.String("vv", "", "Trace verbosity chars")    // Trace Verbosity
-var FlagTraceAfter = flag.Uint64("t", MaxUint64, "Tracing starts after this many steps")
-
-func SetVerbosityBits(s string) {
-	for _, r := range s {
-		if int(r) >= len(V) {
-			log.Panicf("Verbosity rune %d too large for Verbosity Array", r)
-		}
-		if r == 'a' {
-			for i := 'a'; i <= 'z'; i++ {
-				V[i] = true
-			}
-			continue
-		}
-		V[r] = true
-	}
-}
-
 type Word uint16
 
 // EA is Effective Address, which may be a Word or a special value for a register.
