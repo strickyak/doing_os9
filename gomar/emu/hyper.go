fbda99a7a57513ca85e855c1ec070b9d7dab034e
strick <strick@yak.net>
2022-05-29 01:19:08 -0700

    ShowChar/Hex/Ram, in hyper.go

--- gomar/emu/hyper.go
+++ gomar/emu/hyper.go
@@ -8,22 +8,41 @@ import (
 
 func HyperOp(hop byte) {
 	switch hop {
-	case 100:
+	case 100: // Fatal
 		FatalCoreDump()
-	case 101:
+
+	case 101: // Show Frame
 		HFrame()
-	case 102:
+
+	case 102: // Explain MMU
 		fmt.Printf("`MMU[%s]`\n", ExplainMMU())
-	case 103:
+
+	case 103: // ShowHex and tick
 		fmt.Printf("$%x`", dreg)
-	case 104:
-		if ' ' <= dreg && dreg <= '~' {
-			fmt.Printf("%c`", dreg)
-		} else if dreg == 10 || dreg == 13 {
-			fmt.Printf("\n`")
-		} else {
-			fmt.Printf("{%d}`", dreg)
+
+	case 104: // ShowChar and tick
+		if (dreg & 128) != 0 {
+			fmt.Printf("^")
 		}
+		ch := (byte)(dreg & 127)
+		if ' ' <= ch && ch <= '~' {
+			fmt.Printf("%c`", ch)
+		} else if ch == 10 || ch == 13 {
+			fmt.Printf(" (%d)\n`", ch)
+		} else {
+			fmt.Printf("{%d}`", ch)
+		}
+
+	case 105: // Show RAM 32
+		fmt.Printf(" [[%x]]{{", dreg)
+		for i := Word(0); i < 32; i += 2 {
+			fmt.Printf("%04x ", PeekW(dreg+i))
+			if (i&7) == 6 && i < 30 {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Printf("}}` ")
+
 	default:
 		log.Printf("Unknown HyperOp $%x = $d.", hop, hop)
 	}
