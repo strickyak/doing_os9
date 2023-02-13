f524e0ee247e10abac28f0e20b71c6b88823beb6
strick <strick@yak.net>
2023-02-12 16:52:35 -0800

    fix UDP send/recv in cocoio.go

--- gomar/emu/cocoio.go
+++ gomar/emu/cocoio.go
@@ -48,13 +48,13 @@ const (
 
 func assert_w_gt(a Word, b Word) {
 	if a <= b {
-		log.Printf("*** ASSERT FAILED: %d > %d", a, b)
+		log.Fatalf("WIZ: *** ASSERT FAILED: %d > %d", a, b)
 	}
 }
 
 func assert_w_lt(a Word, b Word) {
 	if a >= b {
-		log.Printf("*** ASSERT FAILED: %d < %d", a, b)
+		log.Fatalf("WIZ: *** ASSERT FAILED: %d < %d", a, b)
 	}
 }
 
@@ -102,11 +102,11 @@ func wizReset() {
 func listenUDP(hostport string) *net.UDPConn {
 	addy, err := net.ResolveUDPAddr("udp", hostport)
 	if err != nil {
-		log.Panicf("cannot ResolveUDPAddr: %v", err)
+		log.Panicf("WIZ: cannot ResolveUDPAddr: %v", err)
 	}
 	conn, err := net.ListenUDP("udp", addy)
 	if err != nil {
-		log.Panicf("cannot ListenUDP: %v", err)
+		log.Panicf("WIZ: cannot ListenUDP: %v", err)
 	}
 	return conn
 }
@@ -130,7 +130,7 @@ func GetCocoIO(a Word) byte {
 		wizAddr++
 		return z
 	default:
-		log.Panicf("Not a CocoIO addr: %x", a)
+		log.Panicf("WIZ: Not a CocoIO addr: %x", a)
 		panic(0)
 	}
 }
@@ -148,11 +148,11 @@ func PutCocoIO(a Word, b byte) {
 		wizPut(wizAddr, b)
 		wizAddr++
 	default:
-		log.Panicf("Not a CocoIO addr: %x", a)
+		log.Panicf("WIZ: Not a CocoIO addr: %x", a)
 	}
 }
 func wizPutStatus(a Word, b byte) {
-	log.Panicf("Socket Status is a RO register: %x %x", a, b)
+	log.Panicf("WIZ: Socket Status is a RO register: %x %x", a, b)
 }
 func wizPutInterrupt(a Word, b byte) {
 	x := wizMem[a]
@@ -162,10 +162,10 @@ func wizPutInterrupt(a Word, b byte) {
 func wizPutCommand(a Word, b byte) {
 	base := a - 1
 	k := (a >> 8) - 4
-	log.Printf("wizPutCommand a=%x b=%x base=%x k=%x; sock=%#v", a, b, base, k, sock)
 	assert_w_lt(k, 4)
 	txRing := 0x4000 + 0x800*k
 	rxRing := 0x6000 + 0x800*k
+	Ld("WIZ: wizPutCommand a=%x b=%x base=%x tx=%x rx=%x k=%x; sock=%#v", a, b, base, txRing, rxRing, k, sock)
 	switch b {
 	case 0x01:
 		{ // open
@@ -233,25 +233,48 @@ func wizPutCommand(a Word, b byte) {
 		{ // recv
 			Ld("WIZ: UDP RECV socket %d", k)
 			buf := make([]byte, 1500)
-			size, addr, err := sock[k].conn.ReadFromUDP(buf)
-			Ld("WIZ: UDP RECV socket %d got size $%x addr %v err $%x", k, size, addr, err)
+			size, peer, err := sock[k].conn.ReadFromUDP(buf)
+			Ld("WIZ: UDP RECV socket %d got size $%x peer %v err %v", k, size, peer, err)
 			if err != nil {
 				panic(err)
 			}
-			_ = addr                     // TODO use addr
-			assert_w_gt(Word(size), 2)   // reasonable for now
-			assert_w_lt(Word(size), 700) // reasonable for now
+			assert_w_gt(Word(size), 1)    // reasonable for now
+			assert_w_lt(Word(size), 1500) // reasonable for now
 
-			begin := wizWord(base + TxRd)
-			end := wizWord(base + TxWr)
+			const UDP_RX_HEADER_SIZE = 8
+
+			begin := wizWord(base + RxWr)
+			end := wizWord(base + RxRd)
 			gap := end - begin
 			gap &= 0x7ff // 2K ring buffers.
-			assert_w_gt(gap, Word(size))
+			if gap < 1 {
+				gap = 0x7ff
+			}
+			Ld("WIZ: UDP RECV: begin=%x end=%x gap=%x ... rxRing=%x", begin, end, gap, rxRing)
+			assert_w_gt(gap, Word(size+UDP_RX_HEADER_SIZE))
 
+			addrPort := peer.AddrPort()
+			port := addrPort.Port()
+			addr := addrPort.Addr()
+			a4 := addr.As4()
+
+			wizMem[rxRing+(0x7ff&(begin+0))] = a4[0]
+			wizMem[rxRing+(0x7ff&(begin+1))] = a4[1]
+			wizMem[rxRing+(0x7ff&(begin+2))] = a4[2]
+			wizMem[rxRing+(0x7ff&(begin+3))] = a4[3]
+
+			wizMem[rxRing+(0x7ff&(begin+4))] = (byte)(port >> 8)
+			wizMem[rxRing+(0x7ff&(begin+5))] = (byte)(port >> 0)
+			wizMem[rxRing+(0x7ff&(begin+6))] = (byte)(size >> 8)
+			wizMem[rxRing+(0x7ff&(begin+7))] = (byte)(size >> 0)
+
+			// Copy bytes into the Rx Ring
 			for i := 0; i < size; i++ {
-				p := 0x7ff & (begin + Word(i))
+				p := 0x7ff & (begin + UDP_RX_HEADER_SIZE + Word(i))
 				wizMem[rxRing+p] = buf[i]
 			}
+			// Update the pointer for writing into the Rx Ring
+			putWizWord(base+RxWr, 0x1ff&(begin+UDP_RX_HEADER_SIZE+Word(size)))
 
 			// Set "interrupt" bit for RECV
 			wizMem[base+2] |= (1 << 2) // RECV Interrupt Bit.
